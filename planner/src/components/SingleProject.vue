b2d415c7b3bdab1d6a75891b2bc9832adeee6b23
Eduardo <ealvarez@carminaticonsulting.com>
2021-10-18 20:08:59 -0500

    before fix

--- planner/src/components/SingleProject.vue
+++ planner/src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="project">
-        <div class="actions" @click="">
-            <h3>{{ project.title }}</h3>
+    <div class="project" :class="{ complete: project.complete }">
+        <div class="actions">
+            <h3 @click="hideDetails">{{ project.title }}</h3>
             <div class="icons">
-                <span class="material-icons" >done</span>
                 <span class="material-icons" >edit</span>
-                <span @click="deleteDetails" class="material-icons" >delete</span>
+                <span @click="toggleComplete" class="material-icons">done</span>
+                <span @click="deleteDetails" class="material-icons">delete</span>
             </div>
         </div>
 
@@ -21,7 +21,7 @@
         data() {
             return {
                 isHidden: false,
-                uri: 'http://localhost:3000/projects/' + this.project.id
+                uri: 'http://localhost:3000/projects/' + this.project.id,
             }
         },
         methods:{
@@ -29,9 +29,18 @@
                     this.isHidden = !this.isHidden
                     console.log(this.isHidden)
             },
-            deleteDetails(){
-                fetch(this.uri, {method: 'DELETE'})
+            deleteDetails() {
+                fetch(this.uri, { method: 'DELETE' })
                 .then(() => this.$emit('delete', this.project.id))
+                .catch(err => console.log(err))
+            },
+            toggleComplete() {
+                fetch(this.uri, { 
+                    method: 'PATCH',
+                    headers: { 'Content-Type': 'application/json'},
+                    body: JSON.stringify({ complete: !this.project.complete })
+                 })
+                .then(() => this.$emit('complete', this.project.id))
                 .catch(err => console.log(err))
             }
         }
@@ -39,7 +48,7 @@
 </script>
 
 <style scoped>
-  .project {
+    .project {
     margin: 20px auto;
     background: white;
     padding: 10px 20px;
@@ -51,11 +60,11 @@
     cursor: pointer;
   }
   .actions {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
-   .material-icons {
+  .material-icons {
     font-size: 24px;
     margin-left: 10px;
     color: #bbb;
@@ -63,5 +72,12 @@
   }
   .material-icons:hover {
     color: #777;
+  }
+  /* completed projects */
+  .project.complete {
+    border-left: 4px solid #00ce89;
+  }
+  .project.complete .tick {
+    color: #00ce89;
   }
 </style>
